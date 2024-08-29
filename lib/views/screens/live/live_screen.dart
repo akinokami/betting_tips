fdebf657c699de0f5e13757c5ce088ff950c430d
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-29 12:45:30 +0800

    a

--- lib/views/screens/live/live_screen.dart
+++ lib/views/screens/live/live_screen.dart
@@ -1,8 +1,5 @@
 import 'package:betting_tips/controller/live_controller.dart';
-import 'package:betting_tips/services/api_constant.dart';
 import 'package:betting_tips/utils/app_theme.dart';
-import 'package:betting_tips/views/screens/live/webview_screen.dart';
-import 'package:betting_tips/views/widgets/custom_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -32,78 +29,6 @@ class LiveScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(() => WebViewScreen(
-                            matchUrl: ApiConstant.liveScoreUrl,
-                          ));
-                    },
-                    child: CustomCard(
-                      width: 1.sw * 0.43,
-                      widget: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 12.h),
-                          Icon(
-                            Icons.view_timeline_outlined,
-                            size: 40.sp,
-                            color: AppTheme.premiumColor2,
-                          ),
-                          SizedBox(
-                            height: 10.h,
-                          ),
-                          CustomText(
-                            text: 'scores'.tr,
-                          ),
-                          SizedBox(height: 12.h),
-                        ],
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(() => WebViewScreen(
-                            matchUrl: ApiConstant.liveMatchUrl,
-                          ));
-                    },
-                    child: CustomCard(
-                      width: 1.sw * 0.45,
-                      height: 1.sh * 0.20,
-                      widget: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 12.h),
-                          Icon(
-                            Icons.tv,
-                            size: 40.sp,
-                            color: AppTheme.premiumColor2,
-                          ),
-                          SizedBox(
-                            height: 10.h,
-                          ),
-                          CustomText(
-                            text: 'matches'.tr,
-                          ),
-                          SizedBox(height: 12.h),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              CustomText(
-                text: 'Live tips',
-                size: 15.sp,
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
               Expanded(
                 child: Obx(
                   () => liveController.isLoading.value
